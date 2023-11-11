cbc39fa5be100ed2016d25775fb1331ac9216fe0
Md Rasel Rana <raselges127@gmail.com>
2023-11-11 20:16:10 +0600

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+  
+            //// similar way return the result..............................
+    public function index(){
+       return "<h1>'Hello, Laravel!'.</h1>";
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+
+
+
+
+
+Route::get("/",[UserController::class,"index"]);
